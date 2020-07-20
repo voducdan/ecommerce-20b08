837b9dff6be3bcf8eabebb2784f78bc61a1d0592
voducdan <voducdand99@gmail.com>
2020-07-20 23:03:52 +0700

    Test image path 2

--- config/multer-storage.js
+++ config/multer-storage.js
@@ -1,8 +1,8 @@
 const multer = require('multer');
-
+const path = require('path');
 var storage = multer.diskStorage({
 	destination: function (req, file, cb) {
-		cb(null, 'uploads/images');
+		cb(null, path.join(__dirname, '..', '..', 'uploads/images'));
 	},
 	filename: function (req, file, cb) {
 		cb(null, Date.now() + '_' + file.originalname);
