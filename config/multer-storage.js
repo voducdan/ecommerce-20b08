cbd1446499961968d064c0a30e2e9f1a4f73a536
voducdan <voducdand99@gmail.com>
2020-07-20 23:08:30 +0700

    Test image path 3

--- config/multer-storage.js
+++ config/multer-storage.js
@@ -2,7 +2,7 @@ const multer = require('multer');
 const path = require('path');
 var storage = multer.diskStorage({
 	destination: function (req, file, cb) {
-		cb(null, path.join(__dirname, '..', '..', 'uploads/images'));
+		cb(null, path.join(__dirname, '..', 'uploads/images'));
 	},
 	filename: function (req, file, cb) {
 		cb(null, Date.now() + '_' + file.originalname);
